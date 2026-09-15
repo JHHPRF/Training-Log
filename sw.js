cae03ce3a9fb0c2f9eb97be6ebb74dcf257b030c
JHHPRF <jjeshjod@gmail.com>
2026-09-14 21:55:07 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tlog-v2";
+const CACHE = "tlog-v3";
 const CORE = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "icon-splash-512.png"];
 
 self.addEventListener("install", e => {
